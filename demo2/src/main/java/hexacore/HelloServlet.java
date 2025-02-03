449b1d60a1223affb7e919376933acf06e03c4dd
Narendar14082000 <narendarreddypaindla@gmail.com>
2025-02-03 12:17:55 +0530

    login.jsp

---
+++ demo2/src/main/java/hexacore/HelloServlet.java
@@ -0,0 +1,27 @@
+package hexacore;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h1>Welcome to my first Servlet!</h1>");
+	}
+public void destroy() {
+	System.out.println("Servlet Destroyed ....");
+}
+
+}
